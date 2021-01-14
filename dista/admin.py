55ebd4cf30b7a0796c2acc196ec5ca58f6a25b39
uibeom96 <dmlqja7570@daum.net>
2021-01-14 19:07:59 +0900

    dista 모델 설계완료

--- dista/admin.py
+++ dista/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from dista.models import Post, Comment
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("id", "author")
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("id", "author")
--- dista/models.py
+++ dista/models.py
@@ -1,4 +1,35 @@
 from django.db import models
 from base.models import BaseModel
+from django.conf import settings
 
 
+class Post(BaseModel):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="Photo/%Y-%m-%d")
+    hash_tag = models.CharField(max_length=30)
+    like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="likes") 
+    dis_like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="dis_likes") 
+    follower = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="followers")
+    following = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="followings")
+
+    class Meta:
+        ordering = ("-created", )
+        db_table = "post"
+        verbose_name = "작성된 포스트"
+        verbose_name_plural = "모든 포스트"
+
+    def __str__(self):
+        return self.author+"의 글"
+
+class Comment(BaseModel):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    comment = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="comments")
+
+    class Meta:
+        ordering = ("-created", )
+        db_table = "comment"
+        verbose_name = "작성된 댓글"
+        verbose_name_plural = "모든 댓글"
+
+    def __str__(self):
+        return self.author+"님의 댓글"
