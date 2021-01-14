7d7b969d8e03b6ace6e67878ed99d799dfc9557f
uibeom96 <dmlqja7570@daum.net>
2021-01-14 20:09:24 +0900

    post list 구현완료

--- dista/models.py
+++ dista/models.py
@@ -7,10 +7,10 @@ class Post(BaseModel):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     image = models.ImageField(upload_to="Photo/%Y-%m-%d")
     hash_tag = models.CharField(max_length=30)
-    like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="likes") 
-    dis_like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="dis_likes") 
-    follower = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="followers")
-    following = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="followings")
+    like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="likes", blank=True) 
+    dis_like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="dis_likes", blank=True) 
+    follower = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="followers" ,blank=True)
+    following = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="followings", blank=True)
 
     class Meta:
         ordering = ("-created", )
@@ -19,7 +19,7 @@ class Post(BaseModel):
         verbose_name_plural = "모든 포스트"
 
     def __str__(self):
-        return self.author+"의 글"
+        return str(self.author)+"의 글"
 
 class Comment(BaseModel):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -32,4 +32,4 @@ class Comment(BaseModel):
         verbose_name_plural = "모든 댓글"
 
     def __str__(self):
-        return self.author+"님의 댓글"
+        return str(self.author)+"님의 댓글"
--- dista/views.py
+++ dista/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from dista.models import Post
+
 
 @login_required
 def dista_main(request):
-    return render(request, "dista/dista_main.html")
+    post_list = Post.objects.all()
+    return render(request, "dista/dista_main.html", {"post_list": post_list})
 
